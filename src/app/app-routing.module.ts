4e34700f3c1f31d6f539938e67c74b40c13d0896
Narendra Babu Dasari <narendraDasari.dotnet@gmail.com>
2022-04-23 10:54:28 -0500

    Moved  routing into seperate app-arouting.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,64 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './module/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { ContactComponent } from './pages/users/contact/contact.component';
+import { ProfileComponent } from './pages/users/profile/profile.component';
+import { UserDetailsComponent } from './pages/users/user-details/user-details.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      }
+    ]
+  },
+
+  {
+    path: 'user/:id',
+    component: UserDetailsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**', // this matchs to anything
+    component: NotFoundComponent
+  }
+  // {
+  //   path: 'module',
+  //   component: LoginModule,
+  //   children: [
+  //     {
+  //       path: 'module',
+  //       redirectTo: '/login',
+  //       loadChildren: () => import('./module/login/login.component').then(m => m.LoginComponent)
+  //     }
+  //   ],
+
+  // }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { enableTracing: true }) // help in debugging the routes.
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,66 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './pages/users/users.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginModule } from './module/login/login.module';
-import { LoginComponent } from './module/login/login.component';
 import { UserDetailsComponent } from './pages/users/user-details/user-details.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProfileComponent } from './pages/users/profile/profile.component';
 import { ContactComponent } from './pages/users/contact/contact.component';
 
-const routes: Routes = [
-  {
-    path: 'tasks',
-    component: TasksComponent
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      {
-        path: 'profile',
-        component: ProfileComponent
-      },
-      {
-        path: 'contact',
-        component: ContactComponent
-      }
-    ]
-  },
 
-  {
-    path: 'user/:id',
-    component: UserDetailsComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: '**', // this matchs to anything
-    component: NotFoundComponent
-  }
-  // {
-  //   path: 'module',
-  //   component: LoginModule,
-  //   children: [
-  //     {
-  //       path: 'module',
-  //       redirectTo: '/login',
-  //       loadChildren: () => import('./module/login/login.component').then(m => m.LoginComponent)
-  //     }
-  //   ],
-
-  // }
-
-];
 
 
 @NgModule({
@@ -77,7 +28,6 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes, { enableTracing: true }) // help in debugging the routes.
   ],
   providers: [],
   bootstrap: [AppComponent]
